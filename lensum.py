2c9f4f1c03db188ed1f035c64491260486e45650
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2021-05-26 19:31:47 +0545

    Update lensum.py

--- lensum.py
+++ lensum.py
@@ -2,17 +2,4 @@
 
 n,m=map(int,input().split())
  
-def soln(n,m):
- min_sum=0
- for i in range(1,10):
-  for j in range(1,10):
-   sums=i+j # as first sum in ascending order is always min_sum 
-   if(sums==m):
-    min_sum=int(str(i)+str(j))
-    maxs=int(str(min_sum)[::-1])
-    return min_sum,maxs
- 
- return -1 , -1
- 
-n,m=soln(n,m)
-print(n,m)
+#as i only passed 3 tests , i will solve it after few minutes 
