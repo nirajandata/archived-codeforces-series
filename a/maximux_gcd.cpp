caad80cd757091a5435e09712437764394a86463
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2021-10-16 18:39:11 +0545

    Create maximux_gcd.cpp

---
+++ a/maximux_gcd.cpp
@@ -0,0 +1,13 @@
+//https://codeforces.com/gym/348180/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+#define ln "\n"
+#define ll long long
+int main(){
+  int t,n; cin >>t;
+  while(t--){
+	 cin >> n;
+	 cout << n/2 <<ln;
+  }
+  return 0;
+}
