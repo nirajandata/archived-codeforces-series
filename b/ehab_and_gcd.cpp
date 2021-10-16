751dcc697d67f6833eaa79b14e19eab1479abd6e
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2021-10-16 18:41:41 +0545

    Create ehab_and_gcd.cpp

---
+++ b/ehab_and_gcd.cpp
@@ -0,0 +1,14 @@
+//https://codeforces.com/gym/348180/problem/B
+#include <bits/stdc++.h>
+using namespace std;
+#define ln "\n"
+#define ll long long
+int main(){
+  int t; cin >>t;
+  while(t--){
+	 ll   n;
+	 cin >> n;
+	 cout << 1 << " " <<n-1 <<ln; 
+  }
+  return 0;
+}
